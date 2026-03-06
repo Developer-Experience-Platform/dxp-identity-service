be0a5d95cbff0355df940cfffeebf9b4de61bb37
GomesMaurici0 <mauriciosilva.ghomes@gmail.com>
2026-03-06 16:56:36 -0300

    🆙 update: implementando busca completa e update

--- app/src/main/java/com/m_m/dxpidentityservice/controller/IdentityController.java
+++ app/src/main/java/com/m_m/dxpidentityservice/controller/IdentityController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -28,5 +29,16 @@ public class IdentityController {
     public ResponseEntity<IdentityResponse> getIdentityById(@PathVariable UUID id) {
         var response = identityService.getIdentityById(id);
         return ResponseEntity.ok(response);
+    }
+    @GetMapping
+    public ResponseEntity<List<IdentityResponse>> getIdentity() {
+        var ListResponse = identityService.getIdentities();
+        return ResponseEntity.status(HttpStatus.OK).body(ListResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateIdentity(@RequestBody @Valid IdentityInput request, @PathVariable UUID id) {
+        identityService.updateIdentity(request, id);
+        return ResponseEntity.noContent().build();
     }
 }
--- app/src/main/java/com/m_m/dxpidentityservice/service/impl/IdentityServiceImpl.java
+++ app/src/main/java/com/m_m/dxpidentityservice/service/impl/IdentityServiceImpl.java
@@ -8,6 +8,7 @@ import com.m_m.dxpidentityservice.service.IdentityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -30,5 +31,18 @@ public class IdentityServiceImpl implements IdentityService {
         var identityEntity = identityRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Identity not found with id: " + id));
         return mapper.toResponse(identityEntity);
+    }
+
+    @Override
+    public List<IdentityResponse> getIdentities() {
+        var identityEntities = identityRepository.findAll();
+        return mapper.toResponseList(identityEntities);
+    }
+
+    @Override
+    public void updateIdentity(IdentityInput request, UUID id) {
+         var existingEntity = identityRepository.findById(id).orElseThrow(() -> new RuntimeException("Identity not found with id: " + id));
+         var updatedEntity = mapper.updateEntityFromInput(request, existingEntity);
+         identityRepository.save(updatedEntity);
     }
 }
